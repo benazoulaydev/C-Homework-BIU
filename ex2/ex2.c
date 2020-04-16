a497399b89bb60858097cb93690d344cf61c27ce
benazoulaydev <benazoulaydev@gmail.com>
2020-04-16 20:43:17 +0300

    Update ex2.c

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,6 +1,6 @@
 /******************************************
 *Student name : Ben Azoulay	
-* Student : 330485269
+* Student :
 * Exercise name : Exercise 2
 ******************************************/
 
